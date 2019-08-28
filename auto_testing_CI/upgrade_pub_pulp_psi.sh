e79d542d64c3b90e84625e3a739c0202ffa048f9
Cara Wang <wlin@redhat.com>
2019-08-28 13:02:06 +0800

    check the ansible.cfg

--- auto_testing_CI/upgrade_pub_pulp_psi.sh
+++ auto_testing_CI/upgrade_pub_pulp_psi.sh
@@ -14,6 +14,7 @@ prepare_and_update_private_key_for_ansible() {
   chmod 700 ${CI3_WORKSPACE}/id_rsa
   sed -i "/defaults\]/a private_key_file=${CI3_WORKSPACE}/id_rsa" ${CI3_WORKSPACE}/ansible.cfg
   sed -i "s/ignore/False/g" ${CI3_WORKSPACE}/ansible.cfg
+  cat ${CI3_WORKSPACE}/ansible.cfg >> /dev/null
 }
 
 initialize_env(){
